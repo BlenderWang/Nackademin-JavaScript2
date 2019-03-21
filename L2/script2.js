f9cc837388049bcb54f390de1f4bbf27b41dc3f8
Shirley Wang <shirleywang527@gmail.com>
2019-03-21 09:10:24 +0100

    a complete sign in form

--- L2/script2.js
+++ L2/script2.js
@@ -1,4 +1,22 @@
 $(document).ready(() => {
+    $('.form-signin').on('submit', (e) => {
+        alert('form submitted');
+        // e.preventDefault();
+    });
+
+    const formValid = {
+        username: false,
+        password: false
+    };
+
+    function checkValidation() {
+        if(formValid.username && formValid.password) {
+            $('#submit').removeAttr('disabled');
+        }else {
+            $('#submit').attr('disabled', true);
+        }
+    }
+
     $('#username').on('input', () => {
         const username = $('#username').val();
 
@@ -12,10 +30,56 @@ $(document).ready(() => {
 
         if(username.length < 3 || username.length > 10) {
             msg('Must be at least 3 characters but no more than 10');
+            formValid.username = false;
+            checkValidation();
         } else {
             hide();
+            formValid.username = true;
+            checkValidation();
+
             const testExp = new RegExp(/^[a-zA-Z0-9]+$/);
-            (!testExp.test(username)) ? msg('Must not have any special characters') : hide();
+            if(!testExp.test(username)) {
+                msg('Must not have any special characters');
+                formValid.username = false;
+                checkValidation();
+            } else {
+                hide();
+                formValid.username = true;
+                checkValidation();
+            }
+        }
+    });
+
+    $('#inputPassword').on('input', () => {
+        const password = $('#inputPassword').val();
+
+        function msg(body) {
+            $('.err-password').text(body).show();
+        }
+
+        function hide() {
+            $('.err-password').hide();
+        };
+
+        if(password.length < 3 || password.length > 20) {
+            msg('Must be at least 3 characters but no more than 10');
+            formValid.password = false;
+            checkValidation();
+        } else {
+            hide();
+            formValid.password = true;
+            checkValidation();
+
+            const testExp = new RegExp(/^(?=.*\d)(?=.*[a-z])[0-9a-z]{3,}$/);  // at least 3 characters where at least 1 of them is a number
+            if(!testExp.test(password)) {
+                msg('Must contain letters and at least 1 number');
+                formValid.password = false;
+                checkValidation();
+            } else {
+                hide();
+                formValid.password = true;
+                checkValidation();
+            }
         }
     });
 });
